b080c7ba0378f596202a34589ba50374e74a7876
hyeonginju <guddls64@naver.com>
2024-06-27 21:53:57 +0900

    todo 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Calculator extends StateNotifier<double> {
-  Calculator() : super(0.0);
+class TodoMachine extends StateNotifier<List<Map<String, dynamic>>> {
+  TodoMachine() : super([]) {
+    loadTodos();
+  }
 
-  void calculateBMI(double kg, double m) {
-    state = double.parse((kg / (m * m) * 10000).toStringAsFixed(1));
+  void loadTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final todos = prefs.getStringList('todos') ?? [];
+    state = todos.map((todo) => {'title': todo, 'completed': false}).toList();
+  }
+
+  void addTodo(String title) async {
+    final prefs = await SharedPreferences.getInstance();
+    state = [
+      ...state,
+      {'title': title, 'completed': false}
+    ];
+    prefs.setStringList(
+        'todos', state.map((todo) => todo['title'] as String).toList());
+  }
+
+  void checkTodo(int index) async {
+    final prefs = await SharedPreferences.getInstance();
+    state = [
+      for (int i = 0; i < state.length; i++)
+        if (i == index)
+          {...state[i], 'completed': !state[i]['completed']}
+        else
+          state[i]
+    ];
+    prefs.setStringList(
+        'todos', state.map((todo) => todo['title'] as String).toList());
+  }
+
+  void removeTodo(int index) async {
+    final prefs = await SharedPreferences.getInstance();
+    state = [
+      for (int i = 0; i < state.length; i++)
+        if (i != index) state[i]
+    ];
+    prefs.setStringList(
+        'todos', state.map((todo) => todo['title'] as String).toList());
   }
 }
 
-final calculatorProvider = StateNotifierProvider<Calculator, double>((ref) {
-  return Calculator();
+final todoProvider =
+    StateNotifierProvider<TodoMachine, List<Map<String, dynamic>>>((ref) {
+  return TodoMachine();
 });
 
 void main() {
@@ -26,94 +65,126 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('BMI Calculator')),
+        appBar: AppBar(title: const Text('Riverpod TODO app')),
         body: const Center(
-          child: BMIWidget(),
+          child: TodoWidget(),
         ),
       ),
     );
   }
 }
 
-class BMIWidget extends ConsumerStatefulWidget {
-  const BMIWidget({super.key});
+class TodoWidget extends ConsumerStatefulWidget {
+  const TodoWidget({super.key});
 
   @override
-  _BMIWidgetState createState() => _BMIWidgetState();
+  _TodoWidgetState createState() => _TodoWidgetState();
 }
 
-class _BMIWidgetState extends ConsumerState<BMIWidget> {
-  late TextEditingController weightController;
-  late TextEditingController heightController;
+class _TodoWidgetState extends ConsumerState<TodoWidget> {
+  late TextEditingController todoController;
 
   @override
   void initState() {
     super.initState();
-    weightController = TextEditingController();
-    heightController = TextEditingController();
+    todoController = TextEditingController();
   }
 
   @override
   void dispose() {
-    weightController.dispose();
-    heightController.dispose();
+    todoController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final double bmi = ref.read(calculatorProvider);
+    double screenWidth = MediaQuery.of(context).size.width;
+    final todos = ref.watch(todoProvider);
 
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        SizedBox(
-          width: 250,
-          child: TextField(
-            controller: weightController,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter your weight in kg',
-              labelText: 'kg',
-            ),
-          ),
-        ),
-        const SizedBox(height: 20),
-        SizedBox(
-          width: 250,
-          child: TextField(
-            controller: heightController,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter your height in cm',
-              labelText: 'cm',
-            ),
-          ),
-        ),
-        const SizedBox(height: 30),
-        ElevatedButton(
-          onPressed: () {
-            final weight = double.tryParse(weightController.text);
-            final height = double.tryParse(heightController.text);
-            if (weight != null && height != null) {
-              ref
-                  .read(calculatorProvider.notifier)
-                  .calculateBMI(weight, height);
-            }
-          },
-          child: const Text('Calculate BMI'),
-        ),
-        const SizedBox(height: 50),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Your BMI ', style: const TextStyle(fontSize: 25)),
-            Text(bmi.toString(),
+            SizedBox(
+              width: screenWidth * 0.65,
+              child: TextFormField(
                 style:
-                    const TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                controller: todoController,
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.add),
+                  border: UnderlineInputBorder(),
+                  hintText: 'Add your TODO',
+                ),
+              ),
+            ),
+            const SizedBox(width: 20),
+            ElevatedButton(
+              onPressed: () {
+                final title = todoController.text;
+                if (title.isNotEmpty) {
+                  ref.read(todoProvider.notifier).addTodo(title);
+                  todoController.clear();
+                }
+              },
+              child: const Text('Add'),
+            ),
           ],
+        ),
+        const SizedBox(height: 50),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: ListView.builder(
+              itemCount: todos.length,
+              itemBuilder: (context, index) {
+                final todo = todos[index];
+
+                return Row(
+                  children: [
+                    SizedBox(
+                      width: screenWidth * 0.8,
+                      child: Row(
+                        children: [
+                          Checkbox(
+                            value: todo['completed'] as bool,
+                            onChanged: (value) {
+                              ref.read(todoProvider.notifier).checkTodo(index);
+                            },
+                          ),
+                          SizedBox(
+                            width: screenWidth * 0.55,
+                            child: Text(
+                              todo['title'] as String,
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: todo['completed'] as bool
+                                    ? Colors.grey
+                                    : Colors.black,
+                                fontWeight: FontWeight.bold,
+                                decoration: todo['completed'] as bool
+                                    ? TextDecoration.lineThrough
+                                    : TextDecoration.none,
+                              ),
+                            ),
+                          ),
+                          Spacer(),
+                          IconButton(
+                            icon: Icon(Icons.close),
+                            onPressed: () {
+                              ref.read(todoProvider.notifier).removeTodo(index);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
         ),
       ],
     );
